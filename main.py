6009dce8b9a0af75f574d3c4c18dd806cba8274d
Matthew <mfuentes@everettcc.edu>
2024-05-19 18:39:13 -0700

    added data

---
+++ main.py
@@ -0,0 +1,3 @@
+mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
+mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
+mission_success = [True, False, True, True, True, True, False]
